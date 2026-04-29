89318226bf1c628bbbbf8192837f56c57da5d30a
Usman Anwar <su91-bcapm-f23-031@superior.edu.pk>
2026-04-29 17:24:14 +0500

    Initial commit for Alarm Clock

---
+++ Alarm Clock/main.py
@@ -0,0 +1,11 @@
+# Python Alarm Clock
+# ==================
+'''
+This is a simple alarm clock application that allows you to set 
+an alarm for a specific time. When the alarm goes off, it will 
+play a sound to wake you up.
+'''
+import time
+import datetime
+# import winsound
+import pygame
